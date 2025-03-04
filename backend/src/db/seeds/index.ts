7dcab41f0e5aeb3dd8b72c7b4f63971908cf5218
Jafar Rezaei <sayjeyhi@gmail.com>
2025-03-04 23:17:03 +0100

    fix: auth schema issue

--- backend/src/db/seeds/index.ts
+++ backend/src/db/seeds/index.ts
@@ -1,9 +1,9 @@
 import 'dotenv/config';
-import { drizzle } from 'drizzle-orm/node-postgres';
-import { eq } from 'drizzle-orm';
-import { user } from '../schema';
-
-const db = drizzle(process.env.DATABASE_URL!);
+// import { drizzle } from 'drizzle-orm/node-postgres';
+// import { eq } from 'drizzle-orm';
+// import { user } from '@/db/schema';
+//
+// const db = drizzle(process.env.DATABASE_URL!);
 
 async function main() {
   // const user: typeof user.$inferInsert = {
--- backend/src/lib/auth.ts
+++ backend/src/lib/auth.ts
@@ -1,12 +1,14 @@
 import { betterAuth } from 'better-auth';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
 import { db } from '@/db';
+import * as schema from '@/db/schema';
 
 export const auth = betterAuth({
   appName: 'AiJobProcessor',
   trustedOrigins: ['http://localhost:5173'],
   database: drizzleAdapter(db, {
     provider: 'pg',
+    schema,
   }),
   emailAndPassword: {
     enabled: true,
